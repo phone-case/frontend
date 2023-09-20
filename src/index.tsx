ec9f3d09166fe1bbec2be96323b3429dba93d97c
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-09-20 17:42:51 +0900

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/main/index';
+import Main from "./pages/main/index"
+import Signup from './pages/signup/index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
+    <Signup />
   </React.StrictMode>
 );

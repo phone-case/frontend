ca2b4ec343c2efba2570ee738ddb2d265c9522fc
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-09-22 14:55:33 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
   return (
     <Router>
       <Routes> {/* 'Switch' 대신 'Routes'를 사용합니다. */}
-        <Route path="/" element={<Login />} /> {/* 'element' 속성을 사용합니다. */}
+        <Route path="/" element={<Main />} /> {/* 'element' 속성을 사용합니다. */}
+        <Route path="/login" element={<Login />} /> {/* 'element' 속성을 사용합니다. */}
         <Route path="/signup" element={<Signup />} /> {/* 'element' 속성을 사용합니다. */}
       </Routes>
     </Router>

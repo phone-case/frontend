a2f368608fe53364a4bca521343e4b675736772c
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-09-22 14:50:16 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // 'Switch' 대신 'Routes'를 사용합니다.
+
+import Login from './pages/login/index';
+import Signup from './pages/signup/index';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes> {/* 'Switch' 대신 'Routes'를 사용합니다. */}
+        <Route path="/" element={<Login />} /> {/* 'element' 속성을 사용합니다. */}
+        <Route path="/signup" element={<Signup />} /> {/* 'element' 속성을 사용합니다. */}
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from "./pages/login/index"
+import App from "./App"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );

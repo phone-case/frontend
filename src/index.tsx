6ebcebbc1bf93e5ea4715f8f1b58a2845eff1d7d
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-09-22 14:17:49 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/login/index';
+import Login from "./pages/login/index"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +8,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Login />
-    <Signup />
   </React.StrictMode>
 );

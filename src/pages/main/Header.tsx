10b5977371bb25c32b701539046ebfe517a6019e
slowtree999 <zxcv828133@gmail.com>
2023-09-25 14:16:22 +0900

    로고 누르면 홈으로

--- src/pages/main/Header.tsx
+++ src/pages/main/Header.tsx
@@ -6,7 +6,9 @@ const Header = () => {
   return (
     <div className="header">
       <div className="black-nav">
-        <div className="logo">폰껍</div>
+        <div className="logo">
+          <Link to="/">폰껍</Link> {/* 메인 페이지로 이동하는 Link */}
+        </div>
         <div id="login-button">
           <Link to="/login">Login</Link>
         </div>

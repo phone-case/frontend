40762a105c2a3e94cfb2ef4ce26030e35c8be148
slowtree999 <zxcv828133@gmail.com>
2023-09-25 14:16:56 +0900

    11

--- src/pages/main/Header.tsx
+++ src/pages/main/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header">
       <div className="black-nav">
         <div className="logo">
-          <Link to="/">폰껍</Link> {/* 메인 페이지로 이동하는 Link */}
+          <Link to="/">폰껍</Link> 
         </div>
         <div id="login-button">
           <Link to="/login">Login</Link>

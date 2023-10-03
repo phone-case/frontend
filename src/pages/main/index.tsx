988eff9c5c35c8a2039e15540670706263041771
slowtree999 <zxcv828133@gmail.com>
2023-10-03 22:58:11 +0900

    슬라이더 css +

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,36 +1,44 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './style.css';
 import Header from '../../components/Header/Header';
 import S1 from './../../components/Slide/s1'; // 슬라이드 1 컴포넌트 가져오기
 import S2 from './../../components/Slide/s2'; // 슬라이드 2 컴포넌트 가져오기
 import S3 from './../../components/Slide/s3'; // 슬라이드 3 컴포넌트 가져오기
 
-const ImageSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const slides = [<S1 />, <S2 />, <S3 />]; // 슬라이드 컴포넌트 배열
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+
+const SlickSlider = () => {
+  const settings = {
+    dots: true, // 페이지 번호 표시
+    infinite: true, // 무한 루프
+    speed: 500, // 슬라이드 전환 속도 (밀리초)
+    slidesToShow: 1, // 화면에 보여질 슬라이드 수
+    slidesToScroll: 1, // 슬라이드를 넘길 때 이동할 슬라이드 수
   };
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  const sliderRef = useRef<Slider>(null);
+
+  const goToSlide = (slideIndex: number) => {
+    sliderRef.current!.slickGoTo(slideIndex);
   };
+  
+  
 
   return (
-    <div>
-      <Header />
-      <div className="image-slider">
-        <button className="prev-button" onClick={prevSlide}>
-          Previous
-        </button>
-        {slides[currentIndex]} {/* 현재 슬라이드 표시 */}
-        <button className="next-button" onClick={nextSlide}>
-          Next
-        </button>
+      <div>
+        <Header/>
+        <div>
+          <Slider {...settings} ref={sliderRef}>
+            <S1 />
+            <S2 />
+            <S3 />
+          </Slider>
+        </div>
       </div>
-    </div>
   );
 };
 
-export default ImageSlider;
+export default SlickSlider;

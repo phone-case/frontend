8d813900ee6995253ed230472e693b15f84b6397
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-10-11 16:46:03 +0900

    1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style.css';
+import styles from './style.module.css'; // styles로 스타일을 가져옵니다.
 import { useUser } from '../UserContext/UserContext';
 
 const Header = () => {
@@ -11,17 +11,17 @@ const Header = () => {
   };
 
   return (
-    <div className="header">
-      <div className="black-nav">
-        <div className="logo"><Link to="/">폰껍</Link></div>
-        <div id="login-button">
+    <div className={styles.header}> {/* styles.header로 클래스를 참조합니다. */}
+      <div className={styles['black-nav']}> {/* 클래스 이름에 하이픈(-)이 있는 경우 []로 감싸주어야 합니다. */}
+        <div className={styles.logo}><Link to="/">폰껍</Link></div>
+        <div className={styles['login-button']}>
           {userName ? (
             <div>
               {userName} <a href='#' onClick={handleLogout}>로그아웃</a>
             </div>
           ) : (
             <Link to="/login">
-            <img src="./img/loginicon.png" alt="Login" style={{ width: '50px', height: '50px' }} /> {/* 이미지 크기 조절 */}
+              <img src="./img/loginicon.png" alt="Login" style={{ width: '50px', height: '50px' }} /> {/* 이미지 크기 조절 */}
             </Link>
           )}
         </div>

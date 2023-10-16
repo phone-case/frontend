7dd3904e067a999e174b47ba1b4bf2e1cf03b4e7
slowtree999 <zxcv828133@gmail.com>
2023-10-16 15:41:31 +0900

    11

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+import './style.css';
 
-function design() {
+
+function Design() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  // 이미지를 선택
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      // 선택한 이미지를 상태에 설정
+      setSelectedImage(selectedFile);
+    }
+  };
+
+
   return (
     <div>
-      <div className='design-left'>
-        <div className='design-space'>
-          1
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
+      />
+      {selectedImage && (
+        <div className='design-mid'>
+          <div className='design-box'>
+            <div className='design-image-box'>
+                <img src={URL.createObjectURL(selectedImage)} alt="선택한 그림" />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default design;
+export default Design;

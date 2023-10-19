3c97fa3b61146ea490492d9247aa0e396cc19fb6
slowtree999 <zxcv828133@gmail.com>
2023-10-19 10:02:00 +0900

    11

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -6,7 +6,7 @@ import './style.css';
 
 function Design() {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
-
+  
 
   // 이미지를 선택
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -25,29 +25,34 @@ function Design() {
         accept="image/*"
         onChange={handleImageChange}
       />
-      <div className="design-mid">
-        <div className="design-box">
-          <div className="design-image-box">
+        <div className="design-mid">
+          <div className="design-box">
+            <div className="design-image-box">
 
-            {selectedImage ? (
-              <Draggable bounds="parent"> 
-                <Resizable
-                  width={200} // 초기 너비
-                  height={200} // 초기 높이
-                >
-                  <div className="select-img">
-                    <img
-                      src={URL.createObjectURL(selectedImage)}
-                      alt="선택한 그림"
-                    />
-                  </div>
-                </Resizable>
-              </Draggable>
-            ) : null}
-            
+              {selectedImage ? (
+                <Draggable 
+                bounds="parent" //움직일수있는 범위 정함
+                
+                > 
+                  
+                  <Resizable
+                    width={200} // 초기 너비
+                    height={200} // 초기 높이
+                  
+                  >
+                    <div className="select-img">
+                      <img
+                        src={URL.createObjectURL(selectedImage)}
+                        alt="선택한 그림"
+                      />
+                    </div>
+                  </Resizable>
+                </Draggable>
+              ) : null}
+              
+            </div>
           </div>
         </div>
-      </div>
     </div>
   );
 }

172a210a166f420687590ac533fc7109f63be0da
slowtree999 <zxcv828133@gmail.com>
2023-10-23 16:25:21 +0900

    11

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -5,10 +5,16 @@ import './style.css';
 
 
 function Design() {
+  // 이미지 파일을 저장
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  // 이미지의 위치를 저장
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  
+  // 리사이징 중인지 여부를 저장
   const [isResizing, setIsResizing] = useState(false);
 
+  // 이미지 선택 시 호출
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
@@ -16,6 +22,7 @@ function Design() {
     }
   };
 
+  // 드래그 중 위치를 업데이트
   const handleDrag = (e: any, data: any) => {
     if (!isResizing) {
       setPosition({ x: data.x, y: data.y });
@@ -34,27 +41,30 @@ function Design() {
         <div className='design-box'>
           <div className='design-img-box'>
             {selectedImage ? (
-              <Draggable bounds="parent" onDrag={handleDrag}>
-                <Resizable
-                  enable={{
-                    top: false, 
-                    right: true, 
-                    bottom: false, 
-                    left: false,
-                    topRight: false, 
-                    bottomRight: true, 
-                    bottomLeft: true, 
-                    topLeft: true,
-                  }}
-                >
-                  <div className="select-img">
-                    <img
-                      src={URL.createObjectURL(selectedImage)}
-                      alt="선택한 그림"
-                    />
-                    <div className="drag-handle"/>
-                  </div>
-                </Resizable>
+              <Draggable 
+                bounds="parent" 
+                onDrag={handleDrag}>
+                
+                  <Resizable
+                    enable={{
+                      top: false, 
+                      right: true, 
+                      bottom: false, 
+                      left: false,
+                      topRight: false, 
+                      bottomRight: true, 
+                      bottomLeft: true, 
+                      topLeft: true,
+                    }}
+                  >
+                    <div className="select-img">
+                      <img
+                        src={URL.createObjectURL(selectedImage)}
+                        alt="선택한 그림"
+                      />
+                      <div className="drag-handle"/>
+                    </div>
+                  </Resizable>
               </Draggable>
                 ) : null}
           </div>

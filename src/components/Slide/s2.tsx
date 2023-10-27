3e8a6a6fd3f078d6b12a61f836a36b0cddfde17f
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-10-27 16:54:40 +0900

    1

--- src/components/Slide/s2.tsx
+++ src/components/Slide/s2.tsx
@@ -1,5 +1,6 @@
 // s1.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './s2.css';
 
 const S2 = () => {
@@ -8,6 +9,11 @@ const S2 = () => {
       <video autoPlay muted loop className="slide-video">
         <source src="./videos/test.mp4" type="video/mp4" />
       </video>
+      <Link to="/create">
+        <button className='create-button'>
+          <span>사진 만들러가기</span>
+        </button>
+      </Link>
     </div>
   );
 };
--- src/components/Slide/s3.tsx
+++ src/components/Slide/s3.tsx
@@ -1,17 +1,18 @@
 // s1.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import'./s3.css'
 
 const S3 = () => {
   return (
     <div className="slide-content">
       {/* 슬라이드 내용 */}
       <img src="./img/phone3.PNG" alt="Slide 3" className="slide-image"/>
-        <Link to="/create">
-              <button className='create-button'>
-                create
-              </button>
-        </Link>
+      <Link to="/design">
+        <button className='create-button'>
+          <span>디자인 하러가기</span>
+        </button>
+      </Link>
     </div>
   );
 };

46589fa8ae2dedbca999f23ef2ec2641e234d012
이효태(HYOTAE LEE) <sou3079@gmail.com>
2023-11-17 10:30:34 +0900

    1

--- src/pages/designtest/ImageList.tsx
+++ src/pages/designtest/ImageList.tsx
@@ -1,51 +1,30 @@
 // ImageList.tsx
 import React from 'react';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import styles from './ImageList.module.css';
 
 interface ImageListProps {
   images: { id: number; src: string; alt: string }[];
   onImageOrderChange: (dragIndex: number, hoverIndex: number) => void;
-  onDeleteImage: (id: number) => void; // Added this line
+  onDeleteImage: (id: number) => void;
 }
-
-const DraggableImage: React.FC<{
-  id: number;
-  src: string;
-  alt: string;
-  index: number;
-  moveImage: (dragIndex: number, hoverIndex: number) => void;
-  onDelete: () => void; // Added this line
-}> = ({ id, src, alt, index, moveImage, onDelete }) => {
-  const [, ref] = useDrag({
-    type: 'IMAGE',
-    item: { id, index },
-  });
-
-  const [, drop] = useDrop({
-    accept: 'IMAGE',
-    hover: (item: any) => {
-      if (item.index !== index) {
-        moveImage(item.index, index);
-        item.index = index;
-      }
-    },
-  });
-
-  return (
-    <div ref={(node) => ref(drop(node))} className={styles.draggableImage}>
-      <img src={src} alt={alt} />
-      <button onClick={onDelete} className={styles.deleteButton}>
-        Delete
-      </button>
-    </div>
-  );
-};
 
 const ImageList: React.FC<ImageListProps> = ({ images, onImageOrderChange, onDeleteImage }) => {
   const moveImage = (dragIndex: number, hoverIndex: number) => {
     onImageOrderChange(dragIndex, hoverIndex);
+  };
+
+  const handleMoveUp = (index: number) => {
+    const originalIndex = images.length - 1 - index; // Calculate the original index
+    if (originalIndex > 0) {
+      moveImage(originalIndex, originalIndex - 1);
+    }
+  };
+
+  const handleMoveDown = (index: number) => {
+    const originalIndex = images.length - 1 - index; // Calculate the original index
+    if (originalIndex < images.length - 1) {
+      moveImage(originalIndex, originalIndex + 1);
+    }
   };
 
   const handleDelete = (id: number) => {
@@ -53,24 +32,26 @@ const ImageList: React.FC<ImageListProps> = ({ images, onImageOrderChange, onDel
   };
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className={styles.imageList}>
-        <h2>Image List</h2>
-        <div className={styles.draggableContainer}>
-          {images.map((image, index) => (
-            <DraggableImage
-              key={image.id}
-              id={image.id}
-              src={image.src}
-              alt={image.alt}
-              index={index}
-              moveImage={moveImage}
-              onDelete={() => handleDelete(image.id)}
-            />
-          ))}
-        </div>
+    <div className={styles.imageList}>
+      <h2>Image List</h2>
+      <div className={styles.draggableContainer}>
+        {images.slice().reverse().map((image, index) => (
+          <div key={image.id} className={styles.draggableImage}>
+            <img src={image.src} alt={image.alt} />
+            <br/>
+            <button onClick={() => handleMoveDown(index)} disabled={index === 0}>
+              Up
+            </button>
+            <button onClick={() => handleMoveUp(index)} disabled={index === images.length - 1}>
+              Down
+            </button>
+            <button onClick={() => handleDelete(image.id)} className={styles.deleteButton}>
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
-    </DndProvider>
+    </div>
   );
 };
 
